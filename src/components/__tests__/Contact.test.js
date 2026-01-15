0b4a6bf35d7396de871d8e558cc89bb4536025e8
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2026-01-15 15:04:47 +0530

    added describe

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -3,38 +3,32 @@ import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
 
-test("should load contact us component", () => {
+describe("Contact Us Page Test Case", () =>{
+    test("should load contact us component", () => {
     render(<Contact/>);
-
     const heading = screen.getByRole("heading");
-
     // Assertion 
     expect(heading).toBeInTheDocument();
-})
+});
 
 test("should load button inside contact component", () => {
     render(<Contact/>);
-
     const button = screen.getByText("Submit");
-
     // Assertion 
     expect(button).toBeInTheDocument();
-})
+});
 
 test("should load placeholder name inside contact component", () => {
     render(<Contact/>);
-
     const inputName = screen.getByPlaceholderText("Name");
-
     // Assertion 
     expect(inputName).toBeInTheDocument();
-})
+});
 
 test("should load 2 input boxes in the Contact component", () => {
     render(<Contact/>);
-
     const inputBoxes = screen.getAllByRole("textbox");
-
     // Assertion 
     expect(inputBoxes.length).not.toBe(3);
-})
+});
+});
